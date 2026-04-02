09f2a5eca24e9f334f82703efe3a03891f2833e6
eli05810581 <n12272469@qut.edu.au>
2026-04-03 01:07:41 +1000

    update IP to new EC2

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.55.115.133:5001', // live (YOUR EC2 IP)
+  baseURL: 'http://52.63.234.111:5001', // live (YOUR EC2 IP)
   headers: { 'Content-Type': 'application/json' },
 });
 
